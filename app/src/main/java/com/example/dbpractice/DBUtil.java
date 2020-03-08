f4bf4c7f4ac6370ee33f4ffef6798f3cfb089d09
Riino <sorphwer@gmail.com>
2020-03-08 15:54:28 +0800

    function done

--- app/src/main/java/com/example/dbpractice/DBUtil.java
+++ app/src/main/java/com/example/dbpractice/DBUtil.java
@@ -1,16 +1,20 @@
 package com.example.dbpractice;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
+import android.util.Log;
 
-import static java.security.AccessController.getContext;
+public class DBUtil {
+    //DBHelper dbHelper;
+    Context context;
 
-public class DBUtil extends SQLiteOpenHelper {
-
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "DBPractice.db";
+    DBUtil(Context con){
+        //DBHelper dbHelper = new DBHelper(context);
+        context=con;
+    }
 
     public static class Record implements BaseColumns {
         public static final String TABLE_NAME = "DBPracticeRecord";
@@ -19,40 +23,116 @@ public class DBUtil extends SQLiteOpenHelper {
         public static final String COLUMN_NAME_PHONE = "phone";
     }
 
+    public void insert(String name,String email,String phone){
+        //check input
+        // Gets the data repository in write mode
+        DBHelper dbHelper = new DBHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        SQLiteDatabase dbCheck=dbHelper.getReadableDatabase();
+        //TODO
+        Cursor c = dbCheck.query(Record.TABLE_NAME,new String [] {Record.COLUMN_NAME_NAME},"username=?",new  String[]{ name },null,null,null,null);
+        if(c!=null){
+            String query2="INSERT INTO "+Record.TABLE_NAME +" VALUES ("+
+                    "'"+name+"',"+
+                    "'"+email+"',"+
+                    phone+
+                    ")";
+            db.execSQL(query2);
+        }
+        else{
+            Log.i("SQL","Same name detected.");
+        }
+        c.close();
+    }
 
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + Record.TABLE_NAME + " (" +
-                    Record._ID + " INTEGER PRIMARY KEY," +
-                    Record.COLUMN_NAME_NAME + " TEXT," +
-                    Record.COLUMN_NAME_EMAIL+"TEXT,"+
-                    Record.COLUMN_NAME_PHONE + " TEXT)";
+    public String read(){
+        DBHelper dbHelper = new DBHelper(context);
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + Record.TABLE_NAME;
-//TODO
+        String result="INI",name,email,number;
+        //SQLiteDatabase db = dbHelper.getWritableDatabase();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
+        Cursor c = db.query(Record.TABLE_NAME, null, null, null, null, null, null);
+        c.moveToFirst();
 
-    public DBUtil(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        if (c.moveToFirst()!=false) {
+            //c.moveToNext();
+            c.moveToFirst();
+            Log.i("SQL","find "+String.valueOf(c.getCount())+" records");
+
+            name=c.getString(0);
+            Log.i("SQL","Find Name "+name);
+            email=c.getString(1);
+            Log.i("SQL","Find email "+email);
+            number=c.getString(2);
+            Log.i("SQL","Find number "+number);
+            result=result+name+email+number;
+            result+="\n";
+
+            while(c.moveToNext()){
+                //name=c.getString(c.getColumnIndex(Record.COLUMN_NAME_NAME));
+                name=c.getString(0);
+                Log.i("SQL","Find Name "+name);
+                email=c.getString(1);
+                Log.i("SQL","Find email "+email);
+                number=c.getString(2);
+                Log.i("SQL","Find number "+number);
+                //email = c.getString(c.getColumnIndex(Record.COLUMN_NAME_EMAIL));
+                //number = c.getString(c.getColumnIndex(Record.COLUMN_NAME_PHONE));
+                result=result+name+email+number;
+                result+="\n";
+            }
+            c.close();
+            return result;
+        }
+        else{
+            Log.i("SQL","Query result is empty");
+            result="Empty table";
+            return result;
+        }
+
+    }
+    public void update(String name,String email,String phone) {
+        DBHelper dbHelper = new DBHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        SQLiteDatabase dbCheck = dbHelper.getReadableDatabase();
+        Cursor c = dbCheck.query(Record.TABLE_NAME, new String[]{Record.COLUMN_NAME_NAME}, "username=?", new String[]{name}, null, null, null, null);
+        if (c != null) {
+            name="'"+name+"'";
+            email="'"+email+"'";
+
+            String sql="UPDATE  " +Record.TABLE_NAME+
+                    " SET "+ Record.COLUMN_NAME_PHONE+" = "+phone+
+                    "WHERE "+"username = " +name;
+            //TODO: i HAVE NO FUCKIN IDEA WHY "no such column: email" HERE.
+            Log.i("SQL",sql);
+            db.execSQL(sql);
+        }
+        else {
+            Log.i("SQL","tired to update info that doesn't exist");
+        }
+        c.close();
+
+    }
+
+    public void delete(String name){
+        DBHelper dbHelper = new DBHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String sql = "DELETE FROM " +Record.TABLE_NAME+
+                " WHERE username = " +"'"+name + "'";
+        db.execSQL(sql);
+
     }
 
 
-
-
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_ENTRIES);
+    /*
+    public static void cursorToStringArray(Cursor c,
+                                           ArrayList<String> arrayList, String columnName) {
+        int columnIndex = c.getColumnIndex(columnName);
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            arrayList.add(c.getString(columnIndex));
+        }
     }
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // This database is only a cache for online data, so its upgrade policy is
-        // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES);
-        onCreate(db);
-    }
-    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        onUpgrade(db, oldVersion, newVersion);
-    }
-
-
-
-
+ }
+ */
 }
--- app/src/main/java/com/example/dbpractice/MainActivity.java
+++ app/src/main/java/com/example/dbpractice/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.dbpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button writeButton,readButton,updateButton,removeButton;
     private EditText nameInput,emailInput,numberInput;
 
     private ScrollView display;
+    private TextView result;
+    private DBUtil db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +34,50 @@ public class MainActivity extends AppCompatActivity {
         numberInput=findViewById(R.id.input_number);
 
         display=findViewById(R.id.scroll_result);
+        result=findViewById(R.id.result_text);
 
 
         //DBUtil dbHelper = new DBUtil(getContext());
+        Log.i("SQL","Check");
+        writeButton.setOnClickListener(this);
+        readButton.setOnClickListener(this);
+        updateButton.setOnClickListener(this);
+        removeButton.setOnClickListener(this);
 
+        //numberInput.addTextChangedListener(this);
 
-
+        db = new DBUtil(MainActivity.this);
+        result.setText("VALUE");
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Read_Button:
+                Log.i("SQL","read clicked");
+                result.setText(db.read());
+                break;
+            case R.id.Write_Button:
+                if(nameInput.getText().toString()=="" || emailInput.getText().toString()=="" || numberInput.getText().toString()==""){
+                    Log.i("SQL","tired insert blank info");
+                }
+                else{
+                    db.insert(nameInput.getText().toString(),emailInput.getText().toString(),numberInput.getText().toString());
+                    Log.i("SQL","data inserted");
+                }
+                break;
+            case R.id.Update_Button:
+                db.update(nameInput.getText().toString(),emailInput.getText().toString(),numberInput.getText().toString());
+                Log.i("SQL","Update clicked");
+                break;
+            case R.id.Remove_Button:
+                db.delete(nameInput.getText().toString());
+                break;
+        }
+    }
+
+
+
+
 }
